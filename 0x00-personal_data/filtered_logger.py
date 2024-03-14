2ce3bcc6cf06287c5fb7809bb2ec277576f83cc4
Nimo08 <nimomohamed800@gmail.com>
2024-03-14 09:37:42 +0300

    PII_FIELDS: list

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -50,7 +50,7 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(PII_FIELDS)
+    formatter = RedactingFormatter(list(PII_FIELDS))
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
