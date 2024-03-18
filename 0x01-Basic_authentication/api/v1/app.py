e2beebef3b7be3802ed5ef3e5b36f76e83a829e6
Nimo08 <nimomohamed800@gmail.com>
2024-03-18 19:09:28 +0300

    Check if ImportError: Literal

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from typing_extensions import Literal
 
 
 app = Flask(__name__)
