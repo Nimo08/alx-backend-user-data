f886ef407a2e613aa4a6946716a4bc5a1b37398e
Nimo08 <nimomohamed800@gmail.com>
2024-03-18 21:03:47 +0300

    Add methods=[GET] to route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
     GET /api/v1/unauthorized
@@ -35,10 +35,10 @@ def unauthorized() -> str:
     abort(401)
 
 
-@app_views.route('/forbidden', strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """
     GET /api/v1/forbidden
-    Raise a 403 error by using abort
+    Raise a 403 error using abort
     """
     abort(403)
