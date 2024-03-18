41879309d508ce9e068d029e4288ff9489249dd0
Nimo08 <nimomohamed800@gmail.com>
2024-03-18 22:18:00 +0300

    Modified app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,35 +5,38 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
-
-try:
-    from typing import Literal
-except ImportError:
-    from typing_extensions import Literal
-
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 auth = None
-auth_type = getenv("AUTH_TYPE")
-if auth_type == "auth":
-    auth = Auth()
-if auth_type == "basic_auth":
+if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+@app.before_request
+def before_request():
+    """Filtering of each request"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if request.path not in excluded_paths:
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+    # Run below checks if authentication is required
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(401)
@@ -48,21 +51,10 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.before_request
-def before_request():
-    """Filtering of each request"""
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
-        if not auth.require_auth(request.path, excluded_paths):
-            return
-        # Run below checks if authentication is required
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
