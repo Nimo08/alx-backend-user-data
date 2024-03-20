1be93cb06e2e01885dd88dc6c51f1fb7eb817493
Nimo08 <nimomohamed800@gmail.com>
2024-03-20 20:00:44 +0300

    Modified session_db_auth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -12,17 +12,25 @@ class SessionDBAuth(SessionExpAuth):
         Creates and stores new instance of UserSession and
         returns the Session ID
         """
-        user = UserSession(user_id=user_id)
-        return user.session_id
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        data = {
+            "user_id": user_id,
+            "session_id": session_id
+        }
+        user = UserSession(**data)
+        user.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """
         Returns the User ID by requesting UserSession in the database
         based on session_id
         """
-        user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            return user_session
+        user_id = UserSession.search({"session_id": session_id})
+        if user_id:
+            return user_id
         return None
 
     def destroy_session(self, request=None):
@@ -33,11 +41,10 @@ class SessionDBAuth(SessionExpAuth):
         if request is None:
             return False
         session_id = self.session_cookie(request)
-        if session_id is None:
+        if not session_id:
             return False
         user_sessions = UserSession.search({"session_id": session_id})
-        if not user_sessions:
-            return False
-        for session in user_sessions:
-            session.delete()
-        return True
+        if user_sessions:
+            user_sessions[0].remove()
+            return True
+        return False
