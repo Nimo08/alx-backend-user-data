c60e20e3cc6d9e71e4074503819fbd201c774dbb
Nimo08 <nimomohamed800@gmail.com>
2024-03-26 12:08:32 +0300

    Check create_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,6 +43,18 @@ class Auth:
         except Exception:
             return False
 
+    def create_session(self, email: str) -> str:
+        """
+        Returns the session ID as a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            user.session_id = session_id
+            return session_id
+        except NoResultFound:
+            pass
+
 
 def _hash_password(password: str) -> bytes:
     """
