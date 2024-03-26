455046b1a3fd60197e4d2b6dbaf6631712bc0855
Nimo08 <nimomohamed800@gmail.com>
2024-03-26 11:46:37 +0300

    Check valid_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,6 +32,16 @@ class Auth:
         user_obj = self._db.add_user(email, hashed_pwd)
         return user_obj
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                password_bytes = password.encode('utf-8')
+            return bcrypt.checkpw(password_bytes, user.hashed_password)
+        except Exception:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """
