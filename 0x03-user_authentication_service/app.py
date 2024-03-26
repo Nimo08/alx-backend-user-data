7bbde2448621fbb890526b5572b0858223213df5
Nimo08 <nimomohamed800@gmail.com>
2024-03-26 11:20:01 +0300

    Check POST /users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
+from auth import Auth
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -14,6 +16,18 @@ def index() -> Response:
     Return a JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """POST /users"""
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
