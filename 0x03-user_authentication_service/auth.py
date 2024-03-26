930328c4d36673b0e718aaf53834ea07ea99619a
Nimo08 <nimomohamed800@gmail.com>
2024-03-26 17:03:28 +0300

    Add try-except in get_reset_password_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,8 +72,11 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token"""
-        user = self._db.find_user_by(email=email)
-        if not user:
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user is None:
             raise ValueError()
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
