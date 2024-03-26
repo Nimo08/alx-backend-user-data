4584d573b56130bacd49e7f21762e207dc311ba1
Nimo08 <nimomohamed800@gmail.com>
2024-03-26 17:17:20 +0300

    Request: request.form.get()

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,9 +81,9 @@ def get_reset_password_token():
 @app.route("/reset_password", methods=['PUT'])
 def update_password():
     """Update password end-point"""
-    email = request.form["email"]
-    reset_token = request.form["reset_token"]
-    new_password = request.form["new_password"]
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
     try:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
